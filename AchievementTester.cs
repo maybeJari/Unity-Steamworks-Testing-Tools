9d749da467d1e031f69e15da82121ee3cd018807
Jari Juvonen <jari@honkertown.studio>
2022-09-13 12:56:52 +0300

    Create AchievementTester.cs

---
+++ AchievementTester.cs
@@ -0,0 +1,42 @@
+using System.Diagnostics;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+using Sirenix.OdinInspector;
+
+//? Testing Achievement ID
+//! TESTING_ACHIEVEMENT_01
+public class AchievementTester : MonoBehaviour
+{
+    [Title("Achievement Tester")]
+    [InfoBox("Check your achievement state, unlock and clear achievements.")]
+    public string AchievementID = "TESTING_ACHIEVEMENT_01";
+
+    [Button("State")]
+    [ButtonGroup("Achievement", 0.5f)]
+    public void IsThisAchivementUnlocked()
+    {
+        var ach = new Steamworks.Data.Achievement(AchievementID);
+        Debug.Log($"Achivement {AchievementID} state: " + ach.State);
+    }
+
+    [Button("Unlock")]
+    [ButtonGroup("Achievement", 0.5f)]
+    public void UnlockAchievement()
+    {
+        var ach = new Steamworks.Data.Achievement(AchievementID);
+        ach.Trigger();
+        Debug.Log($"Achivement {AchievementID} Unlocked!");
+    }
+
+    [Button("Clear")]
+    [ButtonGroup("Achievement", 0.5f)]
+    public void ClearAchievement()
+    {
+        var ach = new Steamworks.Data.Achievement(AchievementID);
+        ach.Clear();
+
+        Debug.Log($"Achivement {AchievementID} Cleared!");
+    }
+}
