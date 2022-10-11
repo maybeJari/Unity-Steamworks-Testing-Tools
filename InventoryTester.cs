b88097a28443e2e6cad36b57d296f91760bea129
Jari Juvonen <jiijiijari@gmail.com>
2022-10-11 11:13:19 +0300

    Update InventoryTester.cs

--- InventoryTester.cs
+++ InventoryTester.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using System.Diagnostics;
 using System.Collections;
@@ -5,27 +6,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using Debug = UnityEngine.Debug;
 using Sirenix.OdinInspector;
+using Steamworks;
 
-public class InventoryTester : MonoBehaviour
-{
+public class InventoryTester : MonoBehaviour {
     [Title("Inventory Tester")]
-    [InfoBox("Generate and Remove Items in your Steam Inventory.")]
+    [InfoBox("Generate and Remove Items on your Steam Inventory.")]
     public int itemID = 100;
     public int itemAmount = 1;
 
     [Button("Generate Item")]
     [ButtonGroup("Inventory", 0.5f)]
-    public void GenerateItemAsync()
-    {
-    Steamworks.SteamInventory.LoadItemDefinitions();
-    var itemDef = Steamworks.SteamInventory.FindDefinition( itemID );
-    var inventoryResult = Steamworks.SteamInventory.GenerateItemAsync( itemDef, itemAmount ).Result;
-    Debug.Log($"{itemAmount} of {itemID} has been generated.");
+    public void GenerateItemAsync() {
+        var itemDef = SteamInventory.FindDefinition(itemID);
+        var inventoryResult = SteamInventory
+            .GenerateItemAsync(itemDef, itemAmount)
+            .Result;
+        Debug.Log($"{itemAmount} of {itemID} has been generated.");
     }
 
     [Button("Remove Item")]
     [ButtonGroup("Inventory", 0.5f)]
     public void ItemRemove() {
-    Debug.Log($"Removing Items Currently not Implemented.");
+        //  Debug.Log($"{itemAmount} of {itemID} has been removed from the inventory.");
+        Debug.Log($"Currently not Supported.");
     }
 }
