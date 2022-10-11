c0430f7752972d2a4b0043b5709ed5e87ecbf3c4
Jari Juvonen <jiijiijari@gmail.com>
2022-10-11 11:12:03 +0300

    Update AchievementTester.cs

--- AchievementTester.cs
+++ AchievementTester.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using Debug = UnityEngine.Debug;
 using Sirenix.OdinInspector;
+using Steamworks;
+using SteamData = Steamworks.Data;
 
 //? Testing Achievement ID
 //! TESTING_ACHIEVEMENT_01
@@ -15,28 +17,24 @@ public class AchievementTester : MonoBehaviour
 
     [Button("State")]
     [ButtonGroup("Achievement", 0.5f)]
-    public void IsThisAchivementUnlocked()
-    {
-        var ach = new Steamworks.Data.Achievement(AchievementID);
+    public void IsThisAchivementUnlocked() {
+        var ach = new SteamData.Achievement(AchievementID);
         Debug.Log($"Achivement {AchievementID} state: " + ach.State);
     }
 
     [Button("Unlock")]
     [ButtonGroup("Achievement", 0.5f)]
-    public void UnlockAchievement()
-    {
-        var ach = new Steamworks.Data.Achievement(AchievementID);
+    public void UnlockAchievement() {
+        var ach = new SteamData.Achievement(AchievementID);
         ach.Trigger();
         Debug.Log($"Achivement {AchievementID} Unlocked!");
     }
 
     [Button("Clear")]
     [ButtonGroup("Achievement", 0.5f)]
-    public void ClearAchievement()
-    {
-        var ach = new Steamworks.Data.Achievement(AchievementID);
+    public void ClearAchievement() {
+        var ach = new SteamData.Achievement(AchievementID);
         ach.Clear();
-
         Debug.Log($"Achivement {AchievementID} Cleared!");
     }
 }
